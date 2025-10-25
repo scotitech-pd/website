d1b9ee9f1a85feaf3909e38701db570e51e52ea0
Abhishek Rathore <abhishek@spacelinkers.com>
2025-10-25 17:09:55 +0530

    update security

--- src/components/solutions/SecurityCompliance.jsx
+++ src/components/solutions/SecurityCompliance.jsx
@@ -8,38 +8,44 @@ import {
 
 export default function SecurityCompliance() {
   return (
-    <section className="bg-[#d9edf7] py-20 px-6 md:px-20 relative overflow-hidden">
+    <section className="bg-[#D4EFFF] py-20 px-6 md:px-20 relative overflow-hidden">
       {/* Background faint text */}
-      <h1 className="absolute text-[8rem]  font-bold text-[#c6e2ef] opacity-40 top-10 left-10 select-none">
-        Security & Compliance
-      </h1>
 
       <div className="max-w-7xl mx-auto relative z-10 grid md:grid-cols-2 gap-12 items-center">
         {/* Left Section */}
-        <div>
-          <h2 className="text-3xl md:text-4xl  font-semibold text-[#013a4a]">
-            Security & Compliance
-          </h2>
-          <h3 className="text-4xl md:text-5xl  font-bold text-[#165a68] mt-4 leading-tight">
+        <div className="py-20">
+          <div className="relative">
+            <p className="absolute text-[4.5rem]  font-semibold text-black/20 font-karla opacity-40 top-1/2 -translate-y-1/2 text-nowrap select-none">
+              Security & Compliance
+            </p>
+            <h2 className="text-3xl font-medium text-black font-lora">
+              Security & Compliance
+            </h2>
+          </div>
+          <h3 className="text-4xl font-medium font-lora text-[#197188] mt-8 leading-tight">
             Built for Security. Backed by Apple.
           </h3>
-          <p className="text-[#1a5a68] text-lg mt-6 leading-relaxed">
+          <p className="text-[#197188] text-base mt-4 leading-5 font-lora">
             AppDeploy leverages Apple Business Manager for distribution,
             ensuring compliance with Apple’s official guidelines.
           </p>
 
-          <button className="mt-8 bg-white text-[#1a5a68] font-medium px-6 py-3 rounded-full shadow-sm border border-gray-200 hover:shadow-md transition flex items-center gap-2">
+          <button className="mt-6 bg-white text-sm text-black font-medium font-lora pl-2 pr-5 py-1 rounded-lg shadow-sm transition relative">
             Securely with AppDeploy
-            <span className="bg-blue-600 text-white rounded-full p-1">
-              <Download size={18} />
-            </span>
+            <div className="absolute right-0 top-1/2 group -translate-y-1/2 translate-x-1/2">
+              <img
+                src="/images/solution/downloadcircle.png"
+                alt=""
+                className="size-7 group-hover:size-8 duration-300"
+              />
+            </div>
           </button>
         </div>
 
         {/* Right Section */}
-        <div className="space-y-12 text-black">
+        <div className="space-y-12 text-black hidden min-[1700px]:block">
           {/* Item 1 */}
-          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative">
+          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative ml-60">
             <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
               <img
                 src="/images/solution/security1.png"
@@ -53,7 +59,7 @@ export default function SecurityCompliance() {
           </div>
 
           {/* Item 2 */}
-          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative">
+          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative ml-40">
             <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
               <img
                 src="/images/solution/security2.png"
@@ -67,12 +73,12 @@ export default function SecurityCompliance() {
           </div>
 
           {/* Item 3 */}
-          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative">
+          <div className="flex items-center bg-white rounded-full shadow-md p-2 hover:shadow-lg transition relative ml-20">
             <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
               <img
                 src="/images/solution/security3.png"
                 alt=""
-                className="h-10"
+                className="h-12"
               />
             </div>
             <p className="ml-16 text-lg  font-medium">
@@ -95,6 +101,64 @@ export default function SecurityCompliance() {
           </div>
         </div>
       </div>
+
+      <div className="space-y-12 text-black absolute right-0 top-32 w-[45%]">
+          {/* Item 1 */}
+          <div className="flex items-center bg-white rounded-l-full shadow-md p-2 hover:shadow-lg transition relative ml-60">
+            <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
+              <img
+                src="/images/solution/security1.png"
+                alt=""
+                className="h-12"
+              />
+            </div>
+            <p className="ml-16 text-lg  font-medium">
+              Officially supported by Apple
+            </p>
+          </div>
+
+          {/* Item 2 */}
+          <div className="flex items-center bg-white rounded-l-full shadow-md p-2 hover:shadow-lg transition relative ml-40">
+            <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
+              <img
+                src="/images/solution/security2.png"
+                alt=""
+                className="h-12"
+              />
+            </div>
+            <p className="ml-16 text-lg  font-medium">
+              Easy revocation to prevent misuse
+            </p>
+          </div>
+
+          {/* Item 3 */}
+          <div className="flex items-center bg-white rounded-l-full shadow-md p-2 hover:shadow-lg transition relative ml-20">
+            <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
+              <img
+                src="/images/solution/security3.png"
+                alt=""
+                className="h-12"
+              />
+            </div>
+            <p className="ml-16 text-lg  font-medium">
+              Audit-ready for compliance teams.
+            </p>
+          </div>
+
+          {/* Item 4 */}
+          <div className="flex items-center bg-white rounded-l-full shadow-md p-2 hover:shadow-lg transition relative">
+            <div className="p-4 rounded-full absolute bg-white -left-5 shadow-[0_0_10px_rgba(100,113,113,0.9)]">
+              <img
+                src="/images/solution/security4.png"
+                alt=""
+                className="h-12"
+              />
+            </div>
+            <p className="ml-16 text-lg  font-medium">
+              Secure code signing & license management.
+            </p>
+          </div>
+        </div>
     </section>
   );
 }
