adb0a3f38c181b9ed62479f6c611352943dff4da
Abhishek Rathore <abhishek@spacelinkers.com>
2025-10-28 12:42:58 +0530

    update

--- src/app/(page)/(products)/opsly/page.jsx
+++ src/app/(page)/(products)/opsly/page.jsx
@@ -1,9 +1,13 @@
+import OpslyFeaturesSection from "@/components/products/opsly/OpslyFeaturesSection"
 import OpslyHeroSection from "@/components/products/opsly/OpslyHeroSection"
+import OpslyInfoSection from "@/components/products/opsly/OpslyInfoSection"
 
 const Opsly = () => {
   return (
     <>
         <OpslyHeroSection/>
+        <OpslyInfoSection/>
+        <OpslyFeaturesSection/>
     </>
   )
 }
