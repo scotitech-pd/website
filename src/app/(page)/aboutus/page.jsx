41ddcb471f43b58aca1986d59ad72f6af461f862
spaceforworklinkers <anjulforwork@gmail.com>
2025-10-29 18:20:12 +0530

    Add HowWeWorks component and related images for enhanced About Us section

--- src/app/(page)/aboutus/page.jsx
+++ src/app/(page)/aboutus/page.jsx
@@ -1,9 +1,10 @@
 import AboutScotiTech from "@/components/aboutus/AboutScotitech";
 import GlobalD from "@/components/aboutus/GlobalD";
+import HowWeWorks from "@/components/aboutus/HowWeWorks";
 import LeadershipTeam from "@/components/aboutus/Leadershipteam";
 import MissionVision from "@/components/aboutus/MissionVision";
 import Timeline from "@/components/aboutus/TimelineData";
-import React from "react";
+
 
 const page = () => {
   return <>
@@ -12,6 +13,7 @@ const page = () => {
   <LeadershipTeam />
   <MissionVision />
   <GlobalD/>
+  <HowWeWorks/>
   </>;
 };
 
