c1afe457e4e61bbdf32dc5c3957862534e7d57da
Abhishek Rathore <abhishek@spacelinkers.com>
2025-10-29 18:25:24 +0530

    done

--- src/app/(page)/(products)/appdeploy/page.jsx
+++ src/app/(page)/(products)/appdeploy/page.jsx
@@ -1,6 +1,8 @@
 import AppDeployInfo from "@/components/products/appdeploy/AppDeployInfo"
 import AppDeploySection from "@/components/products/appdeploy/AppDeploySection"
+import HowAppDeployWorks from "@/components/products/appdeploy/HowAppDeployWorks"
 import KeyFeatures from "@/components/products/appdeploy/KeyFeatures"
+import WhyChooseAppDeploy from "@/components/products/appdeploy/WhyChooseAppDeploy"
 
 const page = () => {
   return (
@@ -8,6 +10,8 @@ const page = () => {
         <AppDeploySection/>  
         <AppDeployInfo/>
         <KeyFeatures/>  
+        <WhyChooseAppDeploy/>
+        <HowAppDeployWorks/>
     </>
   )
 }
