aa9d0e396af3953088e0c22835f2122a026ab183
Abhishek Rathore <abhishek@spacelinkers.com>
2025-10-31 15:36:44 +0530

    update

---
+++ src/app/(page)/(products)/elderconnect/page.jsx
@@ -0,0 +1,15 @@
+import ElderConnectHero from "@/components/products/elderconnect/ElderConnectHero"
+import ElderConnectSection from "@/components/products/elderconnect/ElderConnectSection"
+import ModernHomeSetupSection from "@/components/products/elderconnect/ModernHomeSetupSection"
+
+const ElderConnectPage = () => {
+  return (
+    <>
+        <ElderConnectHero/> 
+        <ElderConnectSection/>   
+        <ModernHomeSetupSection/>
+    </>
+  )
+}
+
+export default ElderConnectPage
